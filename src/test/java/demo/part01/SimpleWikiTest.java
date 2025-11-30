57a45a408f4e0e9de0c92cbf95d051a2785240b7
Student <asiathome30@gmail.com>
2025-11-30 14:30:35 +0300

    les1

--- src/test/java/demo/part01/SimpleWikiTest.java
+++ src/test/java/demo/part01/SimpleWikiTest.java
@@ -13,6 +13,11 @@ public class SimpleWikiTest {
     @Test
     void test02() {
         open("https://ru.wikipedia.org/wiki/Selenium");
-        $("body").shouldHave(text("Selenide"));
+        $("body").shouldHave(text("Селен"));
+    }
+    @Test
+    void test03() {
+        open("https://slqa.ru/cases/SimpleForm/");
+        $("body").shouldHave(text("Томас Фуллер"));
     }
 }
