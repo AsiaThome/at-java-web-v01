33d4b98b8ca5c260271c103029042a43b5ddc4b4
AsiaThome <asiathome30@gmail.com>
2025-12-07 12:42:32 +0300

    part02

---
+++ src/test/java/work/part02/SearchTestWork.java
@@ -0,0 +1,29 @@
+package work.part02;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTestWork {
+    @Test
+    public void testElementSearchMethods() {
+        open("https://slqamsk.github.io/demo/search-demo/");
+
+        By myLocator = By.id("user-password");
+        SelenideElement myElement = Selenide.element(myLocator);
+        myElement.shouldBe(visible);
+
+        element(By.id("user-password")).shouldBe(visible);
+        $(By.id("user-password")).shouldBe(visible);
+
+        $(By.name("user_email")).shouldBe(visible);
+        $(By.className("btn")).shouldBe(visible);
+        $(By.tagName("h3")).shouldBe(visible);
+        $(By.linkText("Регистрация нового пользователя в системе")).shouldBe(visible);
+        $(By.partialLinkText("Контакты")).shouldBe(visible);
+    }
+}
